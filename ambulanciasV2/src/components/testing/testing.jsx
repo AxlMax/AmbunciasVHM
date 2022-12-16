859793d63957728f2556ea9e52763d510c1d865a
diego axsel garcia <diego.garciasi@autonoma.edu.co>
2022-12-16 01:47:29 -0500

    feat: creacion de component global de modal

--- ambulanciasV2/src/components/testing/testing.jsx
+++ ambulanciasV2/src/components/testing/testing.jsx
@@ -1,12 +1,39 @@
+
+import { useRef} from "react";
+
+import MyModal from "../global/modal";
+
 import "./Testing.css"
 
 function Testing() {
-    return (<>
-    <div class = "borderWidth">
 
-    </div>
+    const childRef = useRef(null);
+
+    const handleOpen = () => {
+        childRef.current.style.display = "flex"
+    } 
+
+    const Body = () =>  <>
+            <p>Some text in the Modal Body</p>
+            <p>Some other text...</p>
+    </>
+
+    const Footer = () => <>
+        </>
+    
+  
+    return (<>
+    <h2>Bottom Modal</h2>
+
+        <button id="myBtn" onClick={handleOpen}>Open Modal</button>
+
+        <MyModal 
+            forwardedRef={childRef} 
+            title = {"Modal Header"}
+            Body = {Body}
+            Footer = {Footer}
+        />
     </>
 );
 }
-
 export default Testing;
